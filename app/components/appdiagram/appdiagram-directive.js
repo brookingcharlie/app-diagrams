1b42b5834156afe1178f709f7af91f6a02294186
Charlie Brooking <cbrookin@thoughtworks.com>
2015-10-21 22:00:17 +1000

    make boxes draggable using snap.svg

--- app/components/appdiagram/appdiagram-directive.js
+++ app/components/appdiagram/appdiagram-directive.js
@@ -27,28 +27,14 @@ angular.module('myApp.version.version-directive', [])
       ];
     }
   };
+}])
+
+.directive('appDraggable', [function() {
+  return {
+    restrict: 'A',
+    scope: {},
+    link: function(scope, element, attrs) {
+      Snap(element[0]).drag();
+    }
+  };
 }]);
-
-function startMove(e) {
-  document.documentElement.moveStartClientX = e.clientX;
-  document.documentElement.moveStartClientY = e.clientY;
-  document.documentElement.moveStartTarget = e.target;
-  document.documentElement.moveStartTargetX = parseInt(e.target.getAttribute('x'));
-  document.documentElement.moveStartTargetY = parseInt(e.target.getAttribute('y'));
-  document.documentElement.setAttribute("onmousemove", "moveIt(event)");
-}
-
-function moveIt(e) {
-  var dX = e.clientX - document.documentElement.moveStartClientX;
-  var dY = e.clientY - document.documentElement.moveStartClientY;
-  var group = document.documentElement.moveStartTarget.parentNode;
-  for (var i = 0; i < group.childNodes.length; i++) {
-    if (!group.childNodes[i].setAttribute) continue;
-    group.childNodes[i].setAttribute("x", document.documentElement.moveStartTargetX + dX);
-    group.childNodes[i].setAttribute("y", document.documentElement.moveStartTargetY + dY);
-  }
-}
-
-function endMove(e) {
-  document.documentElement.setAttribute("onmousemove", null)
-}
