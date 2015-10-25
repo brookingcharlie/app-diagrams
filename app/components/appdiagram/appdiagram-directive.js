eb2401f6147d80f2292e75437aa2939d2038c094
Charlie Brooking <cbrookin@thoughtworks.com>
2015-10-25 21:25:06 +1000

    add resizing support (only from right/bottom edges)

--- app/components/appdiagram/appdiagram-directive.js
+++ app/components/appdiagram/appdiagram-directive.js
@@ -1,5 +1,19 @@
 'use strict';
 
+var resizeMargin = 15;
+
+// Transform (x, y) screen coordinates to be relative to SVG element.
+// For explanation, see <http://stackoverflow.com/a/5223921>.
+function getElementPoint(x, y, element) {
+  var svg = element[0].ownerSVGElement;
+  var screenPoint = svg.createSVGPoint();
+  screenPoint.x = x;
+  screenPoint.y = y;
+  var svgPoint = screenPoint.matrixTransform(svg.getScreenCTM().inverse());
+  var svgToElement = svg.getTransformToElement(element[0]);
+  return svgPoint.matrixTransform(svgToElement);
+}
+
 angular.module('myApp.appdiagram.appdiagram-directive', [
   'colorpicker.module'
 ])
@@ -39,17 +53,36 @@ angular.module('myApp.appdiagram.appdiagram-directive', [
   return {
     restrict: 'A',
     link: function(scope, element, attrs) {
-      var pool = scope.$eval(attrs.appDraggable);
+      var object = scope.$eval(attrs.appDraggable);
+      var resizingX;
+      var resizingY;
+      var moving;
       var startX;
       var startY;
+      var startWidth;
+      var startHeight;
       var onstart = function(x, y, e) {
-        startX = pool.x;
-	startY = pool.y;
+        startX = object.x;
+	startY = object.y;
+        startWidth = object.width;
+        startHeight = object.height;
+        var elementPoint = getElementPoint(x, y, element);
+        resizingX = elementPoint.x >= (object.x + object.width - resizeMargin);
+        resizingY = elementPoint.y >= (object.y + object.height - resizeMargin);
+        moving = !(resizingX || resizingY);
       };
       var onmove = function(dx, dy, x, y, e) {
         scope.$apply(function() {
-          pool.x = startX + dx,
-	  pool.y = startY + dy;
+          if (resizingX) {
+            object.width = startWidth + dx;
+          }
+          if (resizingY) {
+            object.height = startHeight + dy;
+          }
+          if (moving) {
+            object.x = startX + dx,
+	    object.y = startY + dy;
+          }
 	});
       };
       var onend = function(e) {
