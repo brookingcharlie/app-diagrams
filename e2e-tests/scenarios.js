2d6a470086729dca3797508a9f2beefbaeb0e72a
Charlie Brooking <cbrookin@thoughtworks.com>
2015-10-29 20:24:15 +1000

    fix e2e-tests

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -3,40 +3,16 @@
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
 
 describe('my app', function() {
-
-
-  it('should automatically redirect to /view1 when location hash/fragment is empty', function() {
-    browser.get('index.html');
-    expect(browser.getLocationAbsUrl()).toMatch("/view1");
-  });
-
-
-  describe('view1', function() {
-
+  describe('index.html', function() {
     beforeEach(function() {
-      browser.get('index.html#/view1');
+      browser.get('index.html');
     });
 
-
-    it('should render view1 when user navigates to /view1', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 1/);
+    it('should render ', function() {
+      expect(element.all(by.css('h1')).first().getText()).
+        toMatch(/Application Diagram/);
+      expect(element.all(by.css('p.node-title')).first().getText()).
+        toMatch(/really-long-name.of.awesome.application/);
     });
-
-  });
-
-
-  describe('view2', function() {
-
-    beforeEach(function() {
-      browser.get('index.html#/view2');
-    });
-
-
-    it('should render view2 when user navigates to /view2', function() {
-      expect(element.all(by.css('[ng-view] p')).first().getText()).
-        toMatch(/partial for view 2/);
-    });
-
   });
 });
